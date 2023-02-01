1b971a4b01558c399f206312c3af9332cc8a9565
Justin Ellis <jdellis490@gmail.com>
2023-01-31 20:03:35 -0700

    Added Jikan API for anime info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-const searchForm = document.querySelector('#searchBar');
-const buttonEl = document.querySelector('#searchBtn');
-const tokenK = 'AIzaSyBOmO9O2FstPY2qYc1OT5fBE7h0GK1yz4Q';
+const searchForm = document.querySelector('#search-bar');
+const buttonEl = document.querySelector('#search-btn');
+const tokenK = 'AIzaSyBOmO9O2FstPY2qYc1OT5fBE7h0GK1yz4Q'; // Restrict when deployed
 let vidIdTag;
 
 searchSubmission = (e) => {
@@ -12,18 +12,21 @@ searchSubmission = (e) => {
         alert('Please enter Anime title!');
         return;
     }
-    onSearch(inputSearchEl);
+    // onSearch(inputSearchEl);
 }
 
 // fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=anime%20trailer&key=AIzaSyBOmO9O2FstPY2qYc1OT5fBE7h0GK1yz4Q')
 // .then((data) => {
 //     console.log(data.json())
 // });
+
+// Embed link for Youtube videos on page
 let embed = (vidIdTag) => {
     const embedLink = 'https://www.youtube.com/embed/' + vidIdTag;
     document.getElementById('vidIframe').src = embedLink;
 }
 
+// Youtube API fetch for videos
 onSearch = (searchValue) => {
     const requestURL = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' + searchValue + '%20anime%20trailer&key=' + tokenK;
     fetch(requestURL)
@@ -41,6 +44,12 @@ onSearch = (searchValue) => {
             console.log(error);
         })
 }
+
+// Jikan API fetch for anime
+fetch('https://api.jikan.moe/v4/anime')
+.then((response) => {
+    console.log(response.json());
+});
 
 // Event listener on button click and runs search function
 buttonEl.addEventListener('click', searchSubmission);
